6a9573835e127efa6834f497b33dcc16745eb51c
Edson Maia <edsonmaia2005@hotmail.com>
2023-04-24 14:25:50 -0300

    Responsividade Container e Carousel
    Responsividade Container e Carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -12,6 +12,29 @@ function Carousel({ children }) {
         adaptiveHeight: true,
         // slidesToShow: 5,
         slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     }
 
     return (
